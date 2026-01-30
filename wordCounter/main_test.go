bc3d25a2aa14547ff8173f7b2b1b212585a4d16d
Mohammad amin louragi <aminluragi.m.a.l@gmail.com>
2026-01-30 18:04:02 +0330

    add flag (count words or lines)

--- wordCounter/main_test.go
+++ wordCounter/main_test.go
@@ -9,7 +9,16 @@ func TestCountWords(test *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4 word5 word6\n")
 
 	exp := 6
-	res := count(b)
+	res := count(b, false)
+	if res != exp {
+		test.Fatalf("Expected %d, got %d instead. \n", exp, res)
+	}
+}
+
+func TestCountLines(test *testing.T) {
+	b := bytes.NewBufferString("word1 word2\n word3 \n word4 word5 word6")
+	exp := 3
+	res := count(b, true)
 	if res != exp {
 		test.Fatalf("Expected %d, got %d instead. \n", exp, res)
 	}
